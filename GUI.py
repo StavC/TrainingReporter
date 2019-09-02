3688a2ace518efdcc6abafbced63fb851cf372fa
Stav Cohen <cohenstav1@gmail.com>
2019-09-02 23:18:34 +0300

    New StackedBar

--- GUI.py
+++ GUI.py
@@ -5,6 +5,9 @@ from tkinter import font
 from User import User
 from DataBase import *
 import matplotlib.pyplot as plt
+import matplotlib.backends.backend_pdf
+import numpy as np
+
 
 
 class GUI():
@@ -52,20 +55,22 @@ class GUI():
         self.weight_label.grid(column=10, row=8, sticky=(E, S))
         self.date_label.grid(column=10, row=9, sticky=(E, S))
         self.current_weight_label.grid(column=10, row=10, sticky=(E, S))
-        self.gender_review_button = Button(self.frame, text="פילוג מין", command=self.gender_review_function)
-        self.gender_review_button.grid(column=9, row=2, sticky=(E, S))
-        self.most_popular_exercise_button = Button(self.frame, text="סטטיסטיקת מתאמנים",
+        #self.gender_review_button = Button(self.frame, text="פילוג מין", command=self.gender_review_function)
+       # self.gender_review_button.grid(column=9, row=2, sticky=(E, S))
+        self.statics_for_trainer_button = Button(self.frame, text="סטטיסטיקת מתאמנים",
                                                    command=self.statics_for_trainer_function)
-        self.most_popular_exercise_button.grid(column=9, row=3, sticky=(E, S))
-        self.single_report_button = Button(self.frame, text="הפקת דוח יחיד", command=self.single_report_function)
-        self.single_report_button.grid(column=2, row=2)
+        self.statics_for_trainer_button.grid(column=9, row=3, sticky=(E, S))
+        self.multi_report_button = Button(self.frame, text="הפקת דוחות נבחרים", command=self.multi_report_function,state="disabled")
+        self.multi_report_button.grid(column=2, row=2)
+        self.improvment_button=Button(self.frame,text="התקדמות בתרגילים",command=self.improvment_function,state="disabled")
+        self.improvment_button.grid(column=2,row=3)
         for child in self.frame.winfo_children(): child.grid_configure(padx=10, pady=5)
 
         # exercises label and listbox with scroller
         exercises = []
         scrollbar = Scrollbar(self.frame)
         scrollbar.grid(column=1, row=2, sticky=(N, S))
-        self.exercises_list = Listbox(self.frame, listvariable=exercises, height=5, yscrollcommand=scrollbar.set)
+        self.exercises_list = Listbox(self.frame, listvariable=exercises,selectmode='multiple', height=5, yscrollcommand=scrollbar.set)
         self.exercises_label = Label(self.frame, text="תרגילים ", font="Helvetica 12 underline")
         scrollbar.config(command=self.exercises_list.yview)
         self.exercises_label.grid(column=0, row=1)
@@ -80,6 +85,8 @@ class GUI():
             create_new_user(self.conn, self.curr_user)  # new user in DB
         add_exercises_records_to_DB(self.conn, self.curr_user)  # add his exercises to DB
         self.full_report_button.config(state="normal")
+        self.biggest_improvment_button.config(state="normal")
+        self.multi_report_button.config(state="normal")
         plot_exercises_from_db(self.conn, self.curr_user)
         self.add_personal_info_to_labels()
 
@@ -106,17 +113,24 @@ class GUI():
                 self.curr_user = User(self.running_user_id, row[0][1], row[0][2], row[0][3], row[0][4], row[0][5],
                                       row[0][6],row[0][7],row[0][8])
                 self.full_report_button.config(state="normal")
+                self.improvment_button.config(state="normal")
+                self.multi_report_button.config(state="normal")
+
                 self.add_personal_info_to_labels()
             else:
                 print("User doesnt exist")
                 messagebox.showerror("Error", "User dosent exist in DataBase")
                 self.curr_user = ""
                 self.full_report_button.config(state="disabled")
+                self.improvment_button.config(state="disabled")
+                self.multi_report_button.config(state="disabled")
                 self.delete_personal_info_from_labels()
         except ValueError:
             messagebox.showerror("Error", "Enter only numbers (ID)")
             self.curr_user = ""
             self.full_report_button.config(state="disabled")
+            self.improvment_button.config(state="disabled")
+            self.multi_report_button.config(state="disabled")
             self.delete_personal_info_from_labels()
             print("ValueError")
 
@@ -151,7 +165,7 @@ class GUI():
         self.current_weight_label.config(text="משקל נוכחי: " + temp)
 
         cur = self.conn.cursor()  # adding exercises to listbox
-        cur.execute("SELECT ExerciseName FROM Exercises WHERE Id=? ", (self.running_user_id,))
+        cur.execute("SELECT ExerciseName FROM Exercises WHERE Id=? ", (self.curr_user.id,))
         rows = cur.fetchall()
         exercises = []
         for row in rows:
@@ -169,49 +183,77 @@ class GUI():
         self.weight_label.config(text="משקל: ")
         self.gender_label.config(text="מין: ")
         self.age_label.config(text="גיל: ")
+        self.date_label.config(text="תאריך התחלה: ")
+        self.current_weight_label.config(text="משקל נוכחי: ")
         self.exercises_list.delete('0', 'end')
 
-    def single_report_function(self):
-        if self.exercises_list.get(ANCHOR):
-            selected = self.exercises_list.get(ANCHOR)
-            print(selected)
-            curr = self.conn.cursor()
-            curr.execute("SELECT Date,Weight FROM Exercises WHERE Id=? AND ExerciseName=?",
-                         (self.curr_user.id, selected,))
-            rows = curr.fetchall()
-            # print(rows)
-            temp_list = []
-            dates_list = []
-            weights_list = []
-            for row in rows:
-                temp_record = Record(row[1], row[0])
-                temp_list.append(temp_record)
-            temp_list.sort(key=lambda date: datetime.strptime(date.date, "%d/%m/%Y"))
-            for record in temp_list:
-                weights_list.append(record.weight)
-                dates_list.append(record.date)
-            plt.plot(dates_list, weights_list, zorder=1)
-            plt.scatter(dates_list, weights_list, s=300,
-                        color='red', zorder=2)
-            plt.suptitle(selected[::-1])
-            plt.show()
+    def multi_report_function(self):
+        if self.exercises_list.curselection():
+            pdf = matplotlib.backends.backend_pdf.PdfPages("SelectedReport.pdf")
+            selected_list=self.exercises_list.curselection()
+            print(selected_list)
+            for i in range(0,len(selected_list)):
+                  selected=self.exercises_list.get(selected_list[i])
+                  curr = self.conn.cursor()
+                  curr.execute("SELECT Date,Weight FROM Exercises WHERE Id=? AND ExerciseName=?",
+                               (self.curr_user.id, selected,))
+                  rows = curr.fetchall()
+                  # print(rows)
+                  temp_list = []
+                  dates_list = []
+                  weights_list = []
+                  for row in rows:
+                      temp_record = Record(row[1], row[0])
+                      temp_list.append(temp_record)
+                  temp_list.sort(key=lambda date: datetime.strptime(date.date, "%d/%m/%Y"))
+                  for record in temp_list:
+                      weights_list.append(record.weight)
+                      dates_list.append(record.date)
+                  fig, ax = plt.subplots()
+                  plt.plot(dates_list, weights_list, zorder=1)
+                  plt.scatter(dates_list, weights_list, s=10,
+                              color='red', zorder=2)
+                  plt.suptitle(selected[::-1])
+                  plt.setp(ax.xaxis.get_majorticklabels(), rotation=90, ha="right")
+                  plt.gcf().subplots_adjust(bottom=0.20)
+                  if len(weights_list) > 100:
+                      fig.set_size_inches(100, 100)
+
+                  elif len(weights_list) > 90:
+                      fig.set_size_inches(90, 90)
+
+                  elif len(weights_list) > 80:
+                      fig.set_size_inches(80, 80)
+
+                  elif len(weights_list) > 70:
+                      fig.set_size_inches(70, 70)
+
+                  elif len(weights_list) > 60:
+                      fig.set_size_inches(60, 60)
+
+                  elif len(weights_list) > 50:
+                      fig.set_size_inches(50, 50)
+
+                  elif len(weights_list) > 40:
+                      fig.set_size_inches(40, 40)
+
+                  elif len(weights_list) > 30:
+                      fig.set_size_inches(30, 30)
+
+                  elif len(weights_list) > 20:
+                      fig.set_size_inches(20, 20)
+
+                  pdf.savefig(fig)
+
+            pdf.close()
+
         else:
             messagebox.showinfo("select an exercise", "please select an exercise from the list")
 
-    def gender_review_function(self):
 
-        curr = self.conn.cursor()
-        curr.execute("SELECT COUNT(DISTINCT Id) FROM Users WHERE Gender=?", ("זכר",))
-        rows = curr.fetchall()
-        male = rows[0][0]
-        curr.execute("SELECT COUNT(DISTINCT Id) FROM Users WHERE Gender=?", ("נקבה",))
-        rows = curr.fetchall()
-        female = rows[0][0]
-        labels = 'רכז', 'הבקנ'
-        fig1, ax1 = plt.subplots()
-        ax1.pie([male, female], labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-        ax1.axis('equal')
-        plt.show()
+
+
+
 
     def statics_for_trainer_function(self):
 
@@ -241,7 +283,9 @@ class GUI():
         axs[0, 0].pie(counts, labels=labels, autopct=make_autopct(counts),
                       shadow=True, startangle=90)
         axs[0, 0].axis('equal')
-        curr = self.conn.cursor()
+        temp = "התרגיל פופולרי ביותר"
+        axs[0, 0].set_title(temp[::-1])
+
         curr.execute("SELECT COUNT(DISTINCT Id) FROM Users WHERE Gender=?", ("זכר",))
         rows = curr.fetchall()
         male = rows[0][0]
@@ -251,7 +295,8 @@ class GUI():
         labels = 'רכז', 'הבקנ'
         axs[1, 1].pie([male, female], labels=labels, autopct=make_autopct([male,female]), shadow=True, startangle=90)
         axs[1, 1].axis('equal')
-
+        temp = "מין"
+        axs[1, 1].set_title(temp[::-1])
 
         curr.execute("SELECT StartDate FROM Users")
         rows=curr.fetchall()
@@ -269,25 +314,65 @@ class GUI():
             counts.append(y)
         axs[0, 1].pie(counts, labels=months, autopct=make_autopct(counts), shadow=True, startangle=90)
         axs[0, 1].axis('equal')
-
-
+        temp="תאריך התחלה"
+        axs[0,1].set_title(temp[::-1])
         curr.execute("SELECT Age FROM Users")
         rows=curr.fetchall()
         my_dict={}
         ages=[]
         counts=[]
         for row in rows:
-
             if row[0] not in my_dict:
                 my_dict[row[0]]=1
             else:
                 my_dict[row[0]]=my_dict[ages]+1
-
         for x,y in my_dict.items():
             ages.append(x)
             counts.append(y)
         axs[1,0].pie(counts,labels=ages,autopct=make_autopct(counts), shadow=True, startangle=90)
         axs[1,0].axis('equal')
-
+        temp="גיל"
+        axs[1,0].set_title(temp[::-1])
         plt.show()
 
+
+    def improvment_function(self):
+        curr=self.conn.cursor()
+        curr.execute("SELECT * FROM Exercises WHERE Id=?", (self.curr_user.id,))
+        rows=curr.fetchall()
+
+
+        my_exercises_dict = defaultdict(ExerciseArray)
+        for row in rows:
+            temp_record = Record(row[3], row[2])
+            if row[1] not in my_exercises_dict:
+                temp_array = ExerciseArray(row[1])
+                temp_array.add_record(temp_record)
+                my_exercises_dict[row[1]] = temp_array
+            else:
+                my_exercises_dict[row[1]].add_record(temp_record)
+
+        self.curr_user.my_exercises = my_exercises_dict
+        for name in self.curr_user.my_exercises:  # sorting by date
+            self.curr_user.my_exercises[name].records_array.sort(key=lambda date: datetime.strptime(date.date, "%d/%m/%Y"))
+        min_list=[]
+        max_list=[]
+        exercises_list_for_min_max=[]
+        for exercise in self.curr_user.my_exercises:
+            temp_weights=self.curr_user.my_exercises[exercise].get_weights()
+            min_list.append(min(temp_weights))
+            exercises_list_for_min_max.append(self.curr_user.my_exercises[exercise].name[::-1])
+            max_list.append(max(temp_weights)-min(temp_weights))
+
+        ind=np.arange(5)
+        width=0.35
+        p1=plt.bar(ind,min_list,width,color="blue")
+        p2=plt.bar(ind,max_list,width,bottom=min_list,color="cornflowerblue")
+
+        plt.ylabel('משקלים'[::-1])
+        plt.title('שיפור במשקל לפי תרגיל'[::-1])
+        plt.xticks(ind, exercises_list_for_min_max)
+        totalmax=((max(max_list))+(max(min_list)))
+        plt.yticks(np.arange(0, totalmax+20, 10))
+        plt.legend((p1[0], p2[0]), ('משקל מינימלי בתרגיל'[::-1], 'משקל מקסימלי בתרגיל'[::-1]))
+        plt.show()
